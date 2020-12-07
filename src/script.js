49b2b5593f8cc1dbccb332ce2781dc8b8fff4271
supercip971 <supercyp971@gmail.com>
2020-12-07 13:28:47 -0400

    sidebar: maintenant on peut changer la taille de la side bar

--- src/script.js
+++ src/script.js
@@ -1,19 +1,37 @@
 "use strict";
-var sidebarResizeHandle = document.getElementById("infobar_resize");
+var html;
+var mousex = 0;
+function init(){
+    var sidebarResizeHandle = document.getElementById("infobar_resize");
 
-sidebarResizeHandle.addEventListener('mousedown', initResize, false);
+    sidebarResizeHandle.addEventListener('mousedown', initResize, false);
+    html = document.getElementById("html_id");
+    onmousemove = function(e){ mousex = e.x}
 
+}
 function initResize(e) {
+    
+    console.trace("hello 1");
     window.addEventListener('mousemove', resize, false);
     window.addEventListener('mouseup', stopResize, false);
     html.classList.add('sidebar-resizing');
 }
 function resize(e) {
-    document.documentElement.style.setProperty('--sidebar-width', (e.clientX - sidebar.offsetLeft) + 'px');
+    var ev = window.Event;
+    console.trace("hello 2");
+    document.documentElement.style.setProperty('--sidebar-width', mousex + 'px');
 }
 //on mouseup remove windows functions mousemove & mouseup
 function stopResize(e) {
+
+    console.trace("hello 3");
     html.classList.remove('sidebar-resizing');
     window.removeEventListener('mousemove', resize, false);
     window.removeEventListener('mouseup', stopResize, false);
+
+}
+
+function insert(a){
+
+    
 }
